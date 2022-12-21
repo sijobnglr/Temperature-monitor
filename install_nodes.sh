cb9f9879a40929ac36d435297cf6ea1c50557631
sijo mathew <50240084+sijobnglr@users.noreply.github.com>
2022-12-22 00:03:20 +0530

    Update install_nodes.sh

--- install_nodes.sh
+++ install_nodes.sh
@@ -1,13 +1,3 @@
 #! /bin/bash
 
-echo "installing node red and required nodes for stpl temperature controller.. please wait"
-&& sudo apt update 
-&& sudo apt upgrade 
-&& sudo apt install i2c-tools
-&& cd ~/.node-red
-&& sudo npm install --unsafe-perm -g node-red-contrib-dht-sensor
-&& npm install node-red-contrib-oled
-&& npm i node-red-dashboard
-&& npm i node-red-node-email
-&& node-red-restart
-&& sudo reboot
+echo "installing node red and required nodes for stpl temperature controller.. please wait" && sudo apt update && sudo apt upgrade && sudo apt install i2c-tools && cd ~/.node-red && sudo npm install --unsafe-perm -g node-red-contrib-dht-sensor && npm install node-red-contrib-oled && npm i node-red-dashboard && npm i node-red-node-email && node-red-restart && sudo reboot
