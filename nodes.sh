b854caa1160248d1af9daacf4c9e61b3e1833647
sijo mathew <50240084+sijobnglr@users.noreply.github.com>
2022-12-22 00:08:40 +0530

    Add files via upload

---
+++ nodes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade && bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) && sudo systemctl enable nodered.service && cd ~ /.node-red && npm install @node-red-contrib-themes/theme-collection && npm install node-red-dashboard && npm install node-red-contrib-oled && npm install node-red-contrib-dht-sensor && npm install node-red-node-email && node-red restart
+
+
